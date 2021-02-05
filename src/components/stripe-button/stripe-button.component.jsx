3c999ae253ee3f83edde3fb5ca2defa797d977b1
Michael McGrorty <michaelmcgrorty2@gmail.com>
2021-02-05 16:42:01 +0000

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51IHWFiEfDHofLnh0LmDhdYiLG7Hyz4vbqb0TxMt92XWNoCyOFIyXbcV2Qt3q3qk1hFVgQyLAkRWvoxPOVEJuEZBw00cXy4Y6h6';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
